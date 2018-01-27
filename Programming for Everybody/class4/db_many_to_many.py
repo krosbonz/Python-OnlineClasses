5fc213c56baa22e6049642df559a2ec0230c8c8b
Michael Hauck <hauckm@outlook.com>
2018-01-27 14:32:35 -0500

    Ch 15 assignment

--- Programming for Everybody/class4/db_many_to_many.py
+++ Programming for Everybody/class4/db_many_to_many.py
@@ -4,6 +4,7 @@ import sqlite3
 conn = sqlite3.connect('rosterdb.sqlite')
 cur = conn.cursor()
 
+# The "executescript" allows multiple SQL commands to run if each is followed by ";"
 cur.executescript('''
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Member;
@@ -29,8 +30,11 @@ CREATE TABLE Member (
 
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'roster_data.json'
+    fname = 'roster_data_sample.json'
 
+# [
+#   [ "Charley", "si110", 1 ],
+#   [ "Mea", "si110", 0 ],
 
 str_data = open(fname).read()
 json_data = json.loads(str_data)
@@ -39,23 +43,24 @@ for entry in json_data:
 
     name = entry[0];
     title = entry[1];
-    role = entry[2];
 
     print((name, title))
 
     cur.execute('''INSERT OR IGNORE INTO User (name)
         VALUES ( ? )''', ( name, ) )
-
+# The "or ignore" keeps the script from erroring if a unique value is duplicated
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
     user_id = cur.fetchone()[0]
-
+# Get the "user_id" to be used in the "Member" table
     cur.execute('''INSERT OR IGNORE INTO Course (title)
         VALUES ( ? )''', ( title, ) )
     cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
     course_id = cur.fetchone()[0]
-
+# Get the "course_id" to be used in the "Member" table
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
-        ( user_id, course_id, role ) )
+        (user_id, course_id) VALUES ( ?, ? )''',
+# The "or replace" is used as an update to that field in the record
+        ( user_id, course_id ) )
 
     conn.commit()
+# Commit puts everything to disk. It can be done each time through, or after the for loop completes
