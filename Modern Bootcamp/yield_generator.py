3fc679a7e211e31e8fdc2b59eebcca2a10d57d21
krosbonz@outlook.com <mhauck@lawrencegeneral.org>
2019-05-17 14:34:50 -0400

    yield generator join split

---
+++ Modern Bootcamp/yield_generator.py
@@ -0,0 +1,23 @@
+# Yield
+
+"""The yield statement suspends function’s execution and sends a value 
+back to caller, but retains enough state to enable function to resume where 
+it is left off. When resumed, the function continues execution immediately 
+after the last yield run. This allows its code to produce a series of values 
+over time, rather them computing them at once and sending them back like a list."""
+
+# A "generator" is any function that contains "yield"
+
+def nextSquare():
+    i = 1; 
+  
+    # An Infinite loop to generate squares  
+    while True:
+        yield i*i                 
+        i += 1  # Next execution resumes from this point      
+  
+# Driver code to test above generator function 
+for num in nextSquare():
+    if num > 100:
+         break    
+    print(num)
