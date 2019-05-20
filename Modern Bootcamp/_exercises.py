d463a0b1f89492988db476cfa59421df649cc522
krosbonz@outlook.com <mhauck@lawrencegeneral.org>
2019-05-20 16:35:32 -0400

    url exercise

--- Modern Bootcamp/_exercises.py
+++ Modern Bootcamp/_exercises.py
@@ -1,10 +1,29 @@
-# Define function, accepts multiple lists, returns True if contains 
-# Python keyword
+# # Define function, accepts multiple lists, returns True if contains 
+# # Python keyword
 
 
-from keyword import iskeyword
+# from keyword import iskeyword
 
-def contains_keyword(*args):
-    return any([x for x in args if iskeyword(x)])
+# def contains_keyword(*args):
+#     return any([x for x in args if iskeyword(x)])
     
         
+
+
+import requests
+import random
+
+url = "https://icanhazdadjoke.com/search"
+
+topic = input("Let me tell you a joke! Give me a topic: ")
+
+response = requests.get(
+    url, headers={"Accept": "application/json"}, 
+    params={"term": topic})
+
+data = response.json()
+num_jokes = len(data["results"])
+
+
+print("There are " + str(num_jokes) + " for the topic of " + topic + "." )
+print(random.sample([data["results"]['joke']], 1))
