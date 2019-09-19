97eeafedfce17248939840cfeb689584c8649a9c
krosbonz@outlook.com <mhauck@lawrencegeneral.org>
2019-09-19 14:46:39 -0400

    New General directory

--- General/function.py
+++ General/function.py
@@ -1,1 +1,14 @@
-# Python
+#! python3
+
+def func_one():
+    print("This is function one")
+
+def func_two():
+    print("This is function two")
+
+def main():
+    func_one()
+    func_two()
+
+main()
+
