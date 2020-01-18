12ca3ad7b8e3fdb813569b818f2e8f32e71ba0eb
Michael Hauck <krosbonz@outlook.com>
2020-01-18 11:36:34 -0500

    Install Env and Django

---
+++ Projects/Django_Test/django_test_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hauck\onedrive\docume~1\vsproj~1\python\projects\django_test\django_test_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
