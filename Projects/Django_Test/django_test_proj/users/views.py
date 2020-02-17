9cda1ffdd5c414657bba637beb3f13157067e0fb
Michael Hauck <krosbonz@outlook.com>
2020-02-17 11:51:16 -0500

    Django profile pics and install Pillow

--- Projects/Django_Test/django_test_proj/users/views.py
+++ Projects/Django_Test/django_test_proj/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
