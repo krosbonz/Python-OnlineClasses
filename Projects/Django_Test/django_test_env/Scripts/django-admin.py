600054d09e08685b26f9761ab4e51202823d4243
krosbonz <krosbonz@outlook.com>
2021-01-23 16:08:17 -0500

    Remove Django env

--- Projects/Django_Test/django_test_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\hauck\onedrive\docume~1\vsproj~1\python\projects\django_test\django_test_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
